e88bfc0528722a947b4b73f184c6a45f9f005a7c
Samuel Bosawer <zam.bosawer@gmail.com>
2023-04-14 11:19:16 +0900

    commit pertama

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sacode-php-i-day-05-2023
 Belajar PHPMyAdmin
+
+## Create database
+    CREATE DATABASE nama_database
---
+++ function.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Koneks Database 
+$koneksi = mysqli_connect($servername, $username, $password);
+
+if(!$koneksi)
+{
+    echo "<br>Koneksi Database Gagal";
+    die;
+}
+
+
+
+$query = "CREATE database db_05_01";
+error_reporting(0);
+if(mysqli_query($koneksi, $query)){
+    echo "Buat Database berhasil";
+}else{
+
+    echo "Buat Database Gagal". mysqli_error($koneksi)
+;    }
+
+echo "<br>Koneksi berhasil";
