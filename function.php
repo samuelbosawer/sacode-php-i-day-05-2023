72ad2b4ad11d25749ed39205d43a35cd3f37d095
Samuel Bosawer <zam.bosawer@gmail.com>
2023-04-14 11:35:29 +0900

    (+) buat menu

--- function.php
+++ function.php
@@ -9,18 +9,17 @@ $koneksi = mysqli_connect($servername, $username, $password);
 if(!$koneksi)
 {
     echo "<br>Koneksi Database Gagal";
-    die;
 }
 
 
 
-$query = "CREATE database db_05_01";
-error_reporting(0);
-if(mysqli_query($koneksi, $query)){
-    echo "Buat Database berhasil";
-}else{
+// $query = "CREATE database db_05_01";
+// error_reporting(0);
+// if(mysqli_query($koneksi, $query)){
+//     echo "Buat Database berhasil";
+// }else{
 
-    echo "Buat Database Gagal". mysqli_error($koneksi)
-;    }
+//     echo "Buat Database Gagal". mysqli_error($koneksi)
+// ;    }
 
-echo "<br>Koneksi berhasil";
+// echo "<br>Koneksi berhasil";
--- index.php
+++ index.php
@@ -2,3 +2,19 @@
     require 'function.php';
     echo "PHP DAY 05 - Belajar PHPmyAdmin";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Testo</h2>
+    <?php 
+        include 'menu.php';
+    ?>
+</body>
+</html>
